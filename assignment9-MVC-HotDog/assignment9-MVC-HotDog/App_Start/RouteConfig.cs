e6b4d11a17e361f42509c79245716d6ba547d7bf
CEWong <c0wong01@louisville.edu>
2015-03-25 18:05:49 -0400

    Corrected Route Config

--- assignment9-MVC-HotDog/assignment9-MVC-HotDog/App_Start/RouteConfig.cs
+++ assignment9-MVC-HotDog/assignment9-MVC-HotDog/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace assignment9_MVC_HotDog
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Profile", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
